641f996e7661d283052898ac6ac3013e7cccbefb
Mathieu Villion <mvillion@gmail.com>
2022-12-22 21:47:43 +0100

    merged f32 and f64 tests

--- test_np_asm.py
+++ test_np_asm.py
@@ -12,53 +12,34 @@ if __name__ == "__main__":
         print("dummy exit")
         sys.exit()
 
-    # test f32 w/ 128 bit instructions__________________________________________
-    data1 = rng.random((12, 8), np.float32)
-    data1[0] = 0
-    data2 = rng.random((12, 8), np.float32)
+    data_type_dict = {"f32": np.float32, "f64": np.float64}
+    inst_type_dict = {"sse": "_mm_", "avx": "_mm256_"}
 
-    test_dict = {"sse": "_mm_", "avx": "_mm256_"}
+    for data_type, dtype in data_type_dict.items():
+        data1 = rng.random((12, 8), dtype)
+        data1[0] = 0
+        data2 = rng.random((12, 8), dtype)
 
-    for key, prefix in test_dict.items():
-        module_name = "np_asm_%s_f32" % key
-        inst_list = getattr(import_module(module_name), "inst_list")
+        for key, prefix in inst_type_dict.items():
+            module_name = "np_asm_%s_%s" % (key, data_type)
+            inst_list = getattr(import_module(module_name), "inst_list")
 
-        for inst_name, n_input, ref_fun in inst_list:
-            if ref_fun is None:
-                continue
+            for inst_name, n_input, ref_fun in inst_list:
+                if ref_fun is None:
+                    continue
 
-            inst = prefix+inst_name
-            fun = getattr(np_asm, inst)
-            if n_input == 1:
-                out = fun(data1)
-                out_ref = ref_fun(data1)
-            else:
-                out = fun(data1, data2)
-                out_ref = ref_fun(data1, data2)
+                inst = prefix+inst_name
+                fun = getattr(np_asm, inst)
+                if n_input == 1:
+                    out = fun(data1)
+                    out_ref = ref_fun(data1)
+                else:
+                    out = fun(data1, data2)
+                    out_ref = ref_fun(data1, data2)
 
-            print("%s: diff is %f" % (inst, (out-out_ref).std().mean()))
+                print("%s: diff is %f" % (inst, (out-out_ref).std().mean()))
 
-    # test f64 w/ 128 bit instructions__________________________________________
-    data1 = rng.random((12, 8), np.float64)
-    data2 = rng.random((12, 8), np.float64)
-
-    from np_asm_sse_f64 import inst_list
-
-    for inst_name, n_input, ref_fun in inst_list:
-        if ref_fun is None:
-            continue
-
-        inst = "_mm_"+inst_name
-        fun = getattr(np_asm, inst)
-        if n_input == 1:
-            out = fun(data1)
-            out_ref = ref_fun(data1)
-        else:
-            out = fun(data1, data2)
-            out_ref = ref_fun(data1, data2)
-
-        print("%s: diff is %f" % (inst, (out-out_ref).std().mean()))
-
+    # __________________________________________________________________________
     data = np.arange(12*16, dtype=np.int8).reshape(12, 16)
 
     out = np_asm._mm_add_epi8(data, data)
